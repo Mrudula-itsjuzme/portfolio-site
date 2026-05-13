5747071670762a1b157cac4c1992e0cae35b3333
Pedamallu Sai Mrudula <mrudulasankar2007@gmail.com>
2026-05-13 22:37:40 +0530

    Import archive entrance styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/library.css";
+import "./styles/entrance.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
