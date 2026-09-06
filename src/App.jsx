8cf0d2fdc27011d119f4aea170c738c34b11d909
Pedamallu Sai Mrudula <mrudulasankar2007@gmail.com>
2026-09-07 02:29:52 +0530

    Mount portfolio evidence dock

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import ErrorBoundary from "./components/ErrorBoundary";
+import CredibilityDock from "./components/CredibilityDock";
 
 export default function App() {
   return (
     <ErrorBoundary>
       <Home />
+      <CredibilityDock />
     </ErrorBoundary>
   );
 }
