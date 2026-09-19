5ad9f50f56afeaa882f3995f0fc12e4465778107
Pedamallu Sai Mrudula <mrudulasankar2007@gmail.com>
2026-09-19 11:46:24 +0530

    fix: close rebuilt hierarchy sections correctly

--- src/components/lab/LabPage.jsx
+++ src/components/lab/LabPage.jsx
@@ -1002,7 +1002,6 @@ export default function LabPage() {
               ))}
             </div>
           </section>
-          </section>
         </main>
 
         {/* ---------------- footer ---------------- */}
